b532597d135d3e0cf6b8d9fc3f27189c54d86460
g1ammi93 <giammariacarelli93@hotmail.it>
2024-02-05 20:31:37 +0000

    Added api

--- src/components/characters/CharactersCard.vue
+++ src/components/characters/CharactersCard.vue
@@ -1,8 +1,22 @@
 <script>
 
 export default {
-    name: 'Characters Card'
+    name: 'Characters Card',
+    props: {
+        name: String,
+        image: String,
+        type: String,
+        number: Number
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <div class="character-card text-center p-2">
+        <img :src="image" :alt="name" class="img-fluid rounded-circle mb-3">
+        <div> {{ number }}</div>
+        <h4> {{ name }}</h4>
+
+        <div> {{ type }}</div>
+    </div>
+</template>
--- src/components/characters/CharactersList.vue
+++ src/components/characters/CharactersList.vue
@@ -1,11 +1,15 @@
 <script>
 import axios from 'axios';
 const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
+import CharactersCard from './CharactersCard.vue';
 export default {
     name: 'Characters List',
     data: () => ({
         characters: []
     }),
+    components: {
+        CharactersCard
+    },
     created() {
         axios.get(endpoint).then(res => {
             this.characters = res.data.docs;
@@ -15,7 +19,12 @@ export default {
 </script>
 
 <template>
-    <section id="characters">
-        <div v-for="character in characters" :key="character.id"> {{ character.name }}</div>
+    <section id="characters" class="my-5">
+        <div class="row g-4 row-cols-4">
+            <div class="col" v-for="character in characters" :key="character.id">
+                <CharactersCard :name="character.name" :image="character.imageUrl" :type="character.type1"
+                    :number="character.number" />
+            </div>
+        </div>
     </section>
 </template>
